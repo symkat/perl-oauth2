e5526c0971e73cbf71e638f65be0a22fafcc37f9
Ben Tilly <btilly@gmail.com>
2013-07-12 15:30:08 -0700

    Fix broken tests.

--- lib/LWP/Authen/OAuth2.pm
+++ lib/LWP/Authen/OAuth2.pm
@@ -289,11 +289,11 @@ LWP::Authen::OAuth2 - Make requests to OAuth2 APIs.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
