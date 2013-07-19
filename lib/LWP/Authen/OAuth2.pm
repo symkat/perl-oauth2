327e4dac9e9c3d66ca99ec5737b70f97fa5eb931
Ben Tilly <btilly@gmail.com>
2013-07-18 18:36:22 -0700

    Fix 2 minor POD formatting mistakes.

--- lib/LWP/Authen/OAuth2.pm
+++ lib/LWP/Authen/OAuth2.pm
@@ -289,11 +289,11 @@ LWP::Authen::OAuth2 - Make requests to OAuth2 APIs.
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
